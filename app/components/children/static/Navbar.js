f3498476fbf286ab01c68e5c0ded374a8f182f0c
Katie <katiearina@gmail.com>
2017-07-17 20:53:42 -0400

    Set up Navbar component. :dancers:

--- app/components/children/static/Navbar.js
+++ app/components/children/static/Navbar.js
@@ -3,25 +3,26 @@ var React = require("react");
 var Navbar = React.createClass({
   render: function() {
     return (
-
       <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-        <buttonclassNameclass="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
 
         <a className="navbar-brand" href="#">Home</a>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
+
           <ul className="navbar-nav">
+
             <li className="nav-item active">
-            <a className="nav-link" href="#">Something<span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="#">Something<span className="sr-only">(current)</span></a>
             </li>
 
             <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
+              <a className="nav-link" href="#">Features</a>
             </li>
 
             <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
+              <a className="nav-link" href="#">Pricing</a>
             </li>
 
             <li className="nav-item dropdown">
@@ -34,12 +35,14 @@ var Navbar = React.createClass({
                 <a className="dropdown-item" href="#">Another action</a>
                 <a className="dropdown-item" href="#">Something else here</a>
               </div>
-
             </li>
+
           </ul>
+
         </div>
       </nav>
-
-    )
+    );
   }
-})
+});
+
+module.exports = Navbar;
