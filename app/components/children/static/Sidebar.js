577b6d0baf3f401bc2ffa78613eff3f0bfbb918a
Katie <katiearina@gmail.com>
2017-07-22 22:11:43 -0400

    Added embedded map to Home component (not styled). :beetle::mushroom:

--- app/components/children/static/Sidebar.js
+++ app/components/children/static/Sidebar.js
@@ -6,6 +6,9 @@ var Sidebar = React.createClass({
       <div className="col-3 sidebar">
         <div id="sidebar-content">
           <h4>Sidebar Test</h4>
+          <hr />
+          <a href="https://nextdoor.com/neighborhood/milanwoods--durham--nc/" target="_blank">Join Nextdoor</a>
+          <hr />
         </div>
       </div> 
     );
