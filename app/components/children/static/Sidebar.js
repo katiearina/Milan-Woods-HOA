43f611f6a15751972660f5e077773b6f59751015
Katie <katiearina@gmail.com>
2017-07-22 21:47:27 -0400

    Updating sidebar styling. :waxing_crescent_moon:

--- app/components/children/static/Sidebar.js
+++ app/components/children/static/Sidebar.js
@@ -3,10 +3,10 @@ var React = require("react");
 var Sidebar = React.createClass({
   render: function() {
     return (
-      <div className="col-3"> 
-        <div className="sidebar"> 
-          <h4>Sidebar Test</h4> 
-        </div> 
+      <div className="col-3 sidebar">
+        <div id="sidebar-content">
+          <h4>Sidebar Test</h4>
+        </div>
       </div> 
     );
   }
