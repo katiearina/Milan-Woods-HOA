751e03ffe06cfac0261bfe1744481bde155373d7
Katie <katiearina@gmail.com>
2017-07-25 21:00:33 -0400

    :evergreen_tree::blossom: Added embedded Google calendar to sidebar. May want to move to its own page.

--- app/components/children/static/Sidebar.js
+++ app/components/children/static/Sidebar.js
@@ -9,6 +9,8 @@ var Sidebar = React.createClass({
           <hr />
           <a href="https://nextdoor.com/neighborhood/milanwoods--durham--nc/" target="_blank">Join Nextdoor</a>
           <hr />
+          <iframe src="https://calendar.google.com/calendar/embed?showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=300&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=milanwoodscalendar%40gmail.com&amp;color=%231B887A&amp;ctz=America%2FNew_York" id="google-calendar"/>
+          <hr />
         </div>
       </div> 
     );
