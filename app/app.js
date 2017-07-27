fe6a156b2aa8173c2e717522e492837acc9426e6
Katie <katiearina@gmail.com>
2017-07-26 22:43:07 -0400

    Working on adding Auth0. Not done yet. :neutral_face:

--- app/app.js
+++ app/app.js
@@ -10,3 +10,25 @@ var routes = require("./config/react-routes");
 // Renders the contents according to the route page.
 ReactDOM.render(routes, document.getElementById("app"));
 // ReactDOM.render(<Main />, document.getElementById("app"));
+
+window.addEventListener('load', function() {
+
+  var webAuth = new auth0.WebAuth({
+    domain: 'katiearina.auth0.com',
+    clientID: 'pndfZiJPcidXoAezvZWQfMLR04rRB9DG',
+    redirectUri: window.location.href + "/callback",
+    audience: 'https://katiearina.auth0.com/userinfo',
+    responseType: 'token id_token',
+    scope: 'openid'
+  });
+
+  console.log(webAuth);
+
+  var loginBtn = document.getElementById('btn-login');
+
+  loginBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    webAuth.authorize();
+  });
+
+});
