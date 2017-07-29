927fac3666101f234b426881a2025147994b9644
Katie <katiearina@gmail.com>
2017-07-28 20:34:04 -0400

    Added default pet image, some governing docs. :flower_playing_cards::cat::dog::camera:

--- app/components/children/Governing-Docs.js
+++ app/components/children/Governing-Docs.js
@@ -5,6 +5,23 @@ var GoverningDocs = React.createClass({
     return (
       <div>
         <h1>Governing Docs go here</h1>
+
+        <div className="card-deck">
+          <div className="card-block">
+            <img className="card-img-top gov-docs" src="./assets/images/ArchitecturalGuidelines-Thumbnail.png" alt="Architectural Guidelines" />
+            <div className="card-block">
+              <p className="card-text">Architectural Guidelines</p>
+            </div>
+          </div>
+
+          <div className="card-block">
+            <img className="card-img-top gov-docs" src="./assets/images/ArticlesOfIncorporation-Thumbnail.png" alt="Articles of Incorporation" />
+            <div className="card-block">
+              <p className="card-text">Architectural Guidelines</p>
+            </div>
+          </div>
+        </div>
+
       </div>
     );
   }
