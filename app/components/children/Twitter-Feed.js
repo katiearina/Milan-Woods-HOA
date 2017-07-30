40ab26706c12bf8cf1afdeb880574b8b118a17b1
_apj_ <adrian.jensen@gmail.com>
2017-07-30 18:55:42 -0400

    fix merge conflicts

--- app/components/children/Twitter-Feed.js
+++ app/components/children/Twitter-Feed.js
@@ -2,48 +2,6 @@ var React = require("react");
 var Timeline = require('react-twitter-widgets').Timeline;
 
 var TwitterFeed = React.createClass({
-<<<<<<< HEAD
-
-    getInitialState: function(){
-        return { tweetList: ""};
-    },
-
-    componentWillMount: function() {
-        helpers.twitterQuery().then(function(response){
-            // console.log(response.data);
-            this.setState({tweetList: response.data})
-        }.bind(this));
-    },
-
-    renderEmpty: function() {
-        return (
-                <div>
-                    <p>No tweets currently present.</p>
-                </div>
-        );
-    },
-
-    renderTweets: function() {
-        return this.state.tweetList.map(function(tweets, index){
-            return (
-                <div key={index}>
-                    <p>Time  : {tweets.created_at} </p>
-                    <p>Test : {tweets.text}</p>
-                </div>
-            );
-        });
-    },
-
-    renderContainer: function(){
-        return (
-            <div >
-                {this.renderTweets()}
-            </div>
-        )
-    },
-
-=======
->>>>>>> 66095c7ef3b8157c04c48347f8db83a9763e19df
   render: function() {
     return (
       <Timeline
--- app/config/external-api-routes.js
+++ app/config/external-api-routes.js
@@ -27,29 +27,6 @@ weather.find({ search: "Durham, NC", degreeType: "F" }, function(err, result) {
 
 };
 
-<<<<<<< HEAD
-exports.getTwitter = function(req, res) {
-  var twitterAccount = "DurhamPoliceNC"
-  var client = new twitter_mod({
-      consumer_key: process.env.CONSUMER_KEY,
-      consumer_secret: process.env.CONSUMER_SECRET,
-      access_token_key: process.env.ACCESS_TOKEN_KEY,
-      access_token_secret: process.env.ACCESS_TOKEN_SECRET
-    });
-
-    var params = {
-        screen_name: twitterAccount,
-        count: 10
-      };
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
-        if (error) throw error;
-        //  console.log(tweets);
-         res.json(tweets);
-
-    });
-
-}
-=======
 // exports.getTwitter = function(req, res) {
 //   var twitterAccount = "DurhamPoliceNC"
 //   var client = new twitter_mod({
@@ -71,5 +48,4 @@ exports.getTwitter = function(req, res) {
 //     });
 
 // }
->>>>>>> 66095c7ef3b8157c04c48347f8db83a9763e19df
 
