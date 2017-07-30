6902a800a1cc5bf62e360d9c2979d12c142e745c
Katie <katiearina@gmail.com>
2017-07-29 20:55:18 -0400

    Componentized Nextdoor & Pay Dues items. :trophy::moneybag:

---
+++ app/components/children/Nextdoor.js
@@ -0,0 +1,15 @@
+var React = require("react");
+
+var Nextdoor = React.createClass({
+  render: function() {
+    return (
+      <div className="sidebar-links">
+        <a href="https://nextdoor.com/neighborhood/milanwoods--durham--nc/" target="_blank">
+          <img id="nextdoor" src="./assets/images/nextdoor-logo.png" alt="Join Nextdoor" />
+        </a>
+      </div>
+    );
+  }
+});
+
+module.exports = Nextdoor;
--- app/components/children/static/Sidebar.js
+++ app/components/children/static/Sidebar.js
@@ -1,5 +1,7 @@
 var React = require("react");
 var Weather = require("../Weather");
+var Nextdoor = require("../Nextdoor");
+var PayDues = require("../PayDues");
 
 var Sidebar = React.createClass({
   render: function() {
@@ -8,12 +10,8 @@ var Sidebar = React.createClass({
         <div id="sidebar-content">
           <h4>Sidebar Test</h4>
             <hr />
-              <a href="https://nextdoor.com/neighborhood/milanwoods--durham--nc/" target="_blank">
-                <img id="nextdoor" src="./assets/images/nextdoor-logo.png" alt="Join Nextdoor" />
-              </a>
-              <a href="http://www.mpmnc.com/dues.html" target="_blank">
-                <i id="pay-dues" className="fa fa-usd" aria-hidden="true" />
-              </a>
+              <Nextdoor />
+              <PayDues />
             <hr />
               <Weather />
         </div>
