d19693aa7452c0c9b7367040988550e19af8f1b8
Katie <katiearina@gmail.com>
2017-07-29 21:07:59 -0400

    Updated calendar default/styling. :flashlight::pushpin:

--- app/components/children/Calendar.js
+++ app/components/children/Calendar.js
@@ -4,7 +4,7 @@ var Calendar = React.createClass({
   render: function() {
     return (
       <div className="calendar-container">
-        <iframe src="https://calendar.google.com/calendar/embed?showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=300&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=milanwoodscalendar%40gmail.com&amp;color=%231B887A&amp;ctz=America%2FNew_York" id="google-calendar"/>
+        <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=milanwoodscalendar%40gmail.com&amp;color=%231B887A&amp;ctz=America%2FNew_York" id="google-calendar" />
       </div>
     );
   }
