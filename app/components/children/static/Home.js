4451f5df0bb57390739202a123a01be27c0544a0
Katie <katiearina@gmail.com>
2017-08-02 20:39:01 -0400

    Added link to Neighbor Spotlight. :flashlight::tv:

--- app/components/children/static/Home.js
+++ app/components/children/static/Home.js
@@ -6,7 +6,7 @@ var Home = React.createClass({
       <div>
         <h2>Welcome to Milan Woods!</h2>
         <p>We are a close-knit 243-home community nestled within a wooded area in east Durham.</p>
-        <p>Our HOA president, Steve McNulty, and neighborhood watch coordinator and board Historian, Rob Corns, were recently named the April 2017 Neighbor Spotlight recipients from the City of Durham &mdash; Read that article here!</p>
+        <p>Our HOA president, Steve McNulty, and neighborhood watch coordinator and board Historian, Rob Corns, were recently named the April 2017 Neighbor Spotlight recipients from the City of Durham &mdash; Read the interview <a href="https://durhamnc.gov/DocumentCenter/Home/View/15487" target="_blank">here</a>!</p>
       </div>
     );
   }
