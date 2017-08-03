8c576b5011837d56b80ab817760bd845c954a5e8
Katie <katiearina@gmail.com>
2017-08-02 21:45:33 -0400

    Updating board member table & authentication styling. :izakaya_lantern::rotating_light:

--- app/components/children/Board-Members.js
+++ app/components/children/Board-Members.js
@@ -6,7 +6,7 @@ var BoardMembers = React.createClass({
     return (
       <div>
         <h2>Current Board Members</h2>
-          <Table id="board-member-table">
+          <Table striped id="board-member-table">
             <thead>
               <tr>
                 <th>Position</th>
@@ -17,49 +17,49 @@ var BoardMembers = React.createClass({
             </thead>
             <tbody>
               <tr>
-                <td>President</td>
+                <td className="position">President</td>
                 <td>Steve McNulty</td>
                 <td>1307 Logan Street</td>
                 <td>November 2018</td>
               </tr>
               <tr>
-                <td>Vice President</td>
+                <td className="position">Vice President</td>
                 <td>Katie Geckle</td>
                 <td>6 Willis Court</td>
                 <td>November 2018</td>
               </tr>
               <tr>
-                <td>Secretary</td>
+                <td className="position">Secretary</td>
                 <td>Cathy Guldner</td>
                 <td>1316 Logan Street</td>
                 <td>&mdash;</td>
               </tr>
               <tr>
-                <td>Treasurer</td>
+                <td className="position">Treasurer</td>
                 <td>Charisse Gilbreath</td>
                 <td>1429 Milan Street</td>
                 <td>&mdash;</td>
               </tr>
               <tr>
-                <td>Historian</td>
+                <td className="position">Historian</td>
                 <td>Rob Corns</td>
                 <td>1307 Logan Street</td>
                 <td>&mdash;</td>
               </tr>
               <tr>
-                <td>Member</td>
+                <td className="position">Member</td>
                 <td>Fleechia Holloway</td>
                 <td>1001 Clifford Drive</td>
                 <td>&mdash;</td>
               </tr>
               <tr>
-                <td>Member</td>
+                <td className="position">Member</td>
                 <td>Lisa Weaver</td>
                 <td>1315 Logan Street</td>
                 <td>&mdash;</td>
               </tr>
               <tr>
-                <td>Member</td>
+                <td className="position">Member</td>
                 <td>Toni Enoch</td>
                 <td>1417 Milan Street</td>
                 <td>&mdash;</td>
