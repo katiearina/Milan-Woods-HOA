cbe6afcf6b6d2d3b088cf4e63c8bc8d332e86360
Katie <katiearina@gmail.com>
2017-08-06 20:22:06 -0400

    Adding Contact page information. :phone:

--- app/components/children/Contact.js
+++ app/components/children/Contact.js
@@ -8,14 +8,13 @@ var Contact = React.createClass({
           <div className="row">
             <div className="col-6">
               <h5 className="contact-header">Contact Katie:</h5>
-                <i className="fa fa-github-square fa-4x purple-icon" aria-hidden="true" />
-                <i className="fa fa-linkedin-square fa-4x purple-icon" aria-hidden="true" />
-                <i className="fa fa-envelope-square fa-4x purple-icon" aria-hidden="true" />
+                <a href="https://github.com/katiearina/" target="_blank"><i className="fa fa-github-square fa-4x purple-icon" aria-hidden="true" /></a>
+                <a href="https://www.linkedin.com/in/katie-geckle/" target="_blank"><i className="fa fa-linkedin-square fa-4x purple-icon" aria-hidden="true" /></a>
+                <a href="mailto:katie.geckle@gmail.com?subject=Hello!"><i className="fa fa-envelope-square fa-4x purple-icon" aria-hidden="true" /></a>
             </div>
             <div className="col-6">
               <h5 className="contact-header">Contact Adrian:</h5>
-                <i className="fa fa-github-square fa-4x purple-icon" aria-hidden="true" />
-                <i className="fa fa-linkedin-square fa-4x purple-icon" aria-hidden="true" />
+                <a href="https://github.com/event--horizon/" target="_blank"><i className="fa fa-github-square fa-4x purple-icon" aria-hidden="true" /></a>
             </div>
           </div>
       </div>
